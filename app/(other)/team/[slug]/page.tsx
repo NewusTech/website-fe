7ffe68fed110c5888580675da317760e4b4605fe
Affan Maulana <affan.maulana2016@students.unila.ac.id>
2024-07-15 17:23:32 +0700

    feat: add teams detail page

--- app/(other)/team/[slug]/page.tsx
+++ app/(other)/team/[slug]/page.tsx
@@ -1,30 +1,219 @@
-import Breadcrumbs from "@/components/shared/Breadcrumbs";
+import Image from "next/image";
 
 export default async function DetailTeamPage({ params }: { params: { slug: string } }) {
   const { slug } = params;
+  const columns = [
+    {
+      imageSrc: '/assets/images/antoni.svg',
+      altText: 'ICT Project Manager',
+      title: 'MANAJER PROYEK TIK',
+      subtitle: 'ICT Project Manager'
+    },
+    {
+      imageSrc: '/assets/images/antoni.svg',
+      altText: 'Second Image Description',
+      title: 'Title for Second Image',
+      subtitle: 'Subtitle for Second Image'
+    },
+    {
+      imageSrc: '/assets/images/antoni.svg',
+      altText: 'Third Image Description',
+      title: 'Title for Third Image',
+      subtitle: 'Subtitle for Third Image'
+    }
+  ];
+  const certifications = [
+    {
+      logoSrc: '/assets/images/antoni.svg',
+      title: 'ICT PROJECT MANAGER (ICT-PM)',
+      organization: 'Badan Nasional Sertifikasi Profesi (BNSP)',
+      issueDate: 'Oct 2023',
+      expireDate: 'Oct 2025',
+      credentialId: 'BNSP-ISP-317-ID',
+    },
+    {
+      logoSrc: '/assets/images/antoni.svg',
+      title: 'PROGRAMER SENIOR',
+      organization: 'Badan Nasional Sertifikasi Profesi (BNSP)',
+      issueDate: 'Oct 2023',
+      expireDate: 'Oct 2025',
+      credentialId: 'BNSP-ISP-317-ID',
+    },
+  ];
+
   return (
-    <section className="max-w-7xl mx-auto md:my-32 mb-6 px-3 md:px-5">
-      <div className="py-2">
-        {/* <Breadcrumbs paths={slug} /> */}
+    <section className="overflow-hidden min-h-screen">
+      <header className="w-full z-10 relative" style={{ background: 'linear-gradient(90deg, #4F4F4F 0%, #959595 100%)' }}>
+        <div className="flex justify-end max-w-6xl 2xl:max-w-7xl py-10">
+          <Image
+            src='/assets/icons/logo.svg'
+            alt="blog header"
+            loading="lazy"
+            width={1350}
+            height={75}
+            className={`w-[400px] object-contain md:h-[305px] xl:h-[200px] h-[200px]`}
+          />
+        </div>
+      </header>
+
+      {/* Foto */}
+      <div className="max-w-6xl mx-auto -mt-[130px] z-10 relative">
+        <Image
+          src='/assets/images/antoni.svg'
+          alt="blog header"
+          loading="lazy"
+          width={550}
+          height={75}
+          className={`w-[200px] object-contain rounded-full`}
+        />
       </div>
-      {slug}
-    </section>
+
+      <div className="max-w-6xl mx-auto bg-[#F4F4F4] pb-10">
+        <div className="bg-gradient-to-r from-gray-700 to-gray-300 pt-10 pb-5 flex justify-between gap-10">
+          <div className="w-9/12 flex flex-col gap-3">
+            <h1 className="text-webJudul font-bold">Muhammad Muttaqin</h1>
+            <p className="text-webSubjudul font-semibold">Fullstack Developer</p>
+            <p className="text-webDesk items-stretch flex">Lebih baik jadi yang terbaik dari yang terbaik, daripada jadi yang terburuk dari yang terbaik. Tapi ingat satu hal, jangan pernah menjadi yang terbaik dari yang terburuk.</p>
+            <div className="font-bold text-webDesk flex flex-col gap-1">
+              <p>Institut Teknologi Sumatera</p>
+              <p>Teknik Informatika, Aug 2008-Aug 2021</p>
+            </div>
+          </div>
+
+          <div className="w-3/12 flex flex-col text-webDesk gap-2">
+            <h1>Join Date  : 18 Agustus 2022</h1>
+            <p className="font-bold">Language</p>
+            <div>
+              <div className="bg-red-500 h-2 w-5"></div>
+              <div className="bg-white h-2 w-5"></div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-white shadow-lg rounded-lg px-10 py-8">
+          <h2 className="text-gray-900 font-extrabold text-webJudul">Contact Info</h2>
+          <div className="flex flex-wrap gap-6 mt-4 w-full">
+            <div className="w-[45%] flex flex-col gap-3">
+              <div className="flex gap-3 items-start">
+                <div>
+                  <Image src='/assets/icons/teams/linkedin.svg' alt="Logo" width={20} height={20} className="w-5 h-5" />
+                </div>
+                <div>
+                  <h3 className="font-semibold">Profile</h3>
+                  <p className="text-webDesk text-[#480DEC]">linkedin.com/in/muhammad-muttaqin</p>
+                </div>
+              </div>
+              <div className="flex gap-3 items-start">
+                <div>
+                  <Image src='/assets/icons/teams/email.svg' alt="Logo" width={20} height={20} className="w-5 h-5" />
+                </div>
+                <div>
+                  <h3 className="font-semibold">Email</h3>
+                  <p className="text-webDesk text-[#480DEC]">Muttaqin@gmail.com</p>
+                </div>
+              </div>
+            </div>
+            <div className="w-[45%] flex flex-col gap-3">
+              <div className="flex gap-3 items-start">
+                <div>
+                  <Image src='/assets/icons/teams/addres.svg' alt="Logo" width={20} height={20} className="w-5 h-5" />
+                </div>
+                <div>
+                  <h3 className="font-semibold">Address</h3>
+                  <p className="text-webDesk text-[#480DEC]">Jl. Salim Batubara No.118</p>
+                </div>
+              </div>
+              <div>
+                <div className="flex gap-3 items-start">
+                  <div>
+                    <Image src='/assets/icons/teams/bday.svg' alt="Logo" width={20} height={20} className="w-5 h-5" />
+                  </div>
+                  <div>
+                    <h3 className="font-semibold">Birthday</h3>
+                    <p className="text-webDesk">August 09</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Certi */}
+        <div className="bg-white shadow-lg rounded-lg px-10 my-8 py-8">
+          <h2 className="text-gray-900 font-extrabold text-webJudul pb-5">Certificate</h2>
+
+          <div className="grid grid-cols-3 gap-x-5">
+            {columns.map((column, index) => (
+              <div key={index} className="border rounded-md shadow-sm">
+                <Image
+                  src={column.imageSrc}
+                  alt={column.altText}
+                  loading="lazy"
+                  width={550}
+                  height={75}
+                  className={`w-full h-[270px] object-contain rounded-t-md bg-gray`}
+                />
+                <div className="px-10 py-5">
+                  <h1 className="text-webSubjudul font-semibold">{column.title}</h1>
+                  <h2 className="text-webSubjudul">{column.subtitle}</h2>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Licenses & certifications */}
+        <div className="bg-white shadow-lg rounded-lg px-10 my-8 py-8">
+          <h2 className="text-gray-900 font-extrabold text-webJudul pb-5">Licenses & certifications</h2>
+
+          <div className="space-y-4">
+            {certifications.map((certification, index) => (
+              <CertificationCard key={index} certification={certification} />
+            ))}
+          </div>
+        </div>
+
+        {/* Skills */}
+        <div className="bg-white shadow-lg rounded-lg px-10 my-8 py-8">
+          <h2 className="text-gray-900 font-extrabold text-webJudul pb-5">Skills</h2>
+        </div>
+
+        {/* Projects */}
+        <div className="bg-white shadow-lg rounded-lg px-10 my-8 py-8">
+          <h2 className="text-gray-900 font-extrabold text-webJudul pb-5">Projects</h2>
+        </div>
+      </div>
+    </section >
   );
 };
 
-const getIconSrc = (title: any) => {
-  switch (title.toLowerCase()) {
-    case 'facebook':
-      return '/assets/icons/black/fb.svg';
-    case 'twitter':
-      return '/assets/icons/black/twitter.svg';
-    case 'linkedin':
-      return '/assets/icons/black/linkedin.svg';
-    case 'instagram':
-      return '/assets/icons/black/instagram.svg';
-    case 'tiktok':
-      return '/assets/icons/black/tiktok.svg';
-    default:
-      return '/assets/icons/black/default.svg';
-  }
+const CertificationCard = ({ certification }: any) => {
+  return (
+    <div className="border rounded-md shadow-sm p-4 flex items-start">
+      <div className="w-2/12">
+        <Image
+          src={certification.logoSrc}
+          alt="Certification logo"
+          width={50}
+          height={50}
+          className="mr-4"
+        />
+      </div>
+      <div className="w-10/12">
+        <h1 className="text-webJudul font-semibold">{certification.title}</h1>
+        <h2 className="text-webSubjudul font-semibold">{certification.organization}</h2>
+        <p className="text-webDesk">Issued {certification.issueDate}. Expires {certification.expireDate}</p>
+        <p className="text-webDesk">Credential ID {certification.credentialId}</p>
+        <button className="mt-2 px-4 py-2 border rounded-md text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white flex items-center gap-3">
+          <p>Show credential</p>
+          <Image
+            src='/assets/icons/teams/Vector (19).svg'
+            alt="Certification logo"
+            width={15}
+            height={15}
+          />
+        </button>
+      </div>
+    </div>
+  );
 };
