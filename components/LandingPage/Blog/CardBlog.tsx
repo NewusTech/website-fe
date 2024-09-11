e9a192f23d0ed8c76256cf7f3b46f0b6284f7f71
sakatimuna7 <sakatimunapro7@gmail.com>
2024-09-11 09:37:31 +0700

    *update detail portofolio

--- components/LandingPage/Blog/CardBlog.tsx
+++ components/LandingPage/Blog/CardBlog.tsx
@@ -4,23 +4,27 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface BlogProps {
-  id: number,
+  id: number;
   title: string;
-  slug: string,
-  keyword: string,
-  excerpt: string,
-  body: string,
-  altImage: string,
-  kategoriblog_id: number,
-  kategoriblog_title: string,
-  tagblog_id: number,
-  tagblog_title: string,
-  user_id: number,
-  user_title: string,
-  image: any,
-  status: boolean,
-  status_desc: string,
-  publishAt: Date | string,
+  slug: string;
+  keyword: string;
+  excerpt: string;
+  body: string;
+  altImage: string;
+  kategoriblog_id: number;
+  kategoriblog_title: string;
+  tagblog_id: number;
+  tagblog_title: string;
+  user_id: number;
+  user_title: string;
+  image: any;
+  status: boolean;
+  status_desc: string;
+  tags: {
+    id: number;
+    title: string;
+  }[];
+  publishAt: Date | string;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -31,34 +35,50 @@ interface CardBlogProps {
 }
 
 const CardBlog = ({ blogs = {} as BlogProps, type }: CardBlogProps) => {
-  const { id, title, body, image, user_title, publishAt, slug, excerpt, kategoriblog_title, altImage, tagblog_title } = blogs;
+  const {
+    id,
+    title,
+    body,
+    image,
+    user_title,
+    publishAt,
+    slug,
+    excerpt,
+    kategoriblog_title,
+    altImage,
+    tagblog_title,
+    tags,
+  } = blogs;
 
   return (
     <div
       data-aos="fade-up"
       data-aos-duration="1000"
       // style={{ border: '1.6px solid white' }}
-      className={`${type === "landing"
-        ? "bg-[#14141F] text-white flex-row md:flex-col rounded-[6px] md:rounded-[10px] md:p-5 md:w-[100%] w-full h-full md:h-auto shadow gap-3 shadow-gray md:shadow-white"
-        : "bg-transparent flex-col md:w-[410px] h-full rounded-[2px]"
-        } flex`}
+      className={`${
+        type === "landing"
+          ? "bg-[#14141F] text-white flex-row md:flex-col rounded-[6px] md:rounded-[10px] md:p-5 md:w-[100%] w-full h-full md:h-auto shadow gap-3 shadow-gray md:shadow-white"
+          : "bg-transparent flex-col md:w-[410px] h-full rounded-[2px]"
+      } flex`}
     >
       <div
-        className={`${type === "landing"
-          ? "md:w-full w-[40%] md:h-[233px] h-full rounded-l-[6px] md:rounded-[10px]"
-          : "rounded-[2px] md:w-full"
-          }`}
+        className={`${
+          type === "landing"
+            ? "md:w-full w-[40%] md:h-[233px] h-full rounded-l-[6px] md:rounded-[10px]"
+            : "rounded-[2px] md:w-full"
+        }`}
       >
         <Image
-          src={image || '/assets/images/blog.jpg'}
+          src={image || "/assets/images/blog.jpg"}
           alt={altImage || "blog"}
           loading="lazy"
           width={326}
           height={233}
-          className={`${type === "landing"
-            ? "w-full h-full object-cover md:rounded-[10px] rounded-l-[6px]"
-            : "rounded-[2px] md:w-full"
-            }
+          className={`${
+            type === "landing"
+              ? "w-full h-full object-cover md:rounded-[10px] rounded-l-[6px]"
+              : "rounded-[2px] md:w-full"
+          }
           `}
         />
       </div>
@@ -67,9 +87,13 @@ const CardBlog = ({ blogs = {} as BlogProps, type }: CardBlogProps) => {
       >
         <div className="flex items-center justify-between gap-1.5 text-white">
           <div className="flex items-center gap-1 md:gap-2">
-            <h5 className="md:text-webDesk text-mobileDesk capitalize">{user_title ? user_title : 'Lorenka'}</h5>
+            <h5 className="md:text-webDesk text-mobileDesk capitalize">
+              {user_title ? user_title : "Lorenka"}
+            </h5>
             <div className="rounded-full w-[3px] h-[3px] md:w-[5px] md:h-[5px] bg-white"></div>
-            <h5 className="md:text-webDesk text-mobileDesk">{publishAt ? formattedDate(publishAt) : 'January 13, 2024'}</h5>
+            <h5 className="md:text-webDesk text-mobileDesk">
+              {publishAt ? formattedDate(publishAt) : "January 13, 2024"}
+            </h5>
           </div>
           <Link className="block md:hidden" href={`/${slug}`}>
             <Image
@@ -83,8 +107,11 @@ const CardBlog = ({ blogs = {} as BlogProps, type }: CardBlogProps) => {
           </Link>
         </div>
         <div className="flex items-start w-full md:gap-2 my-1 md:my-3 justify-between">
-          <Link className="font-medium text-white md:text-webSubjudul text-mobileSubjudul w-full md:w-11/12 hover:underline line-clamp-2" href={`/${slug}`}>
-            {title ? title : 'Lorem Ipsum'}
+          <Link
+            className="font-medium text-white md:text-webSubjudul text-mobileSubjudul w-full md:w-11/12 hover:underline line-clamp-2"
+            href={`/${slug}`}
+          >
+            {title ? title : "Lorem Ipsum"}
           </Link>
           <Link className="hidden md:flex w-1/12 justify-end" href={`/${slug}`}>
             <Image
@@ -99,15 +126,23 @@ const CardBlog = ({ blogs = {} as BlogProps, type }: CardBlogProps) => {
         <p
           className={`${type === "landing" ? "md:text-webDesk md:w-full" : "w-[142px] md:w-full"} pb-1 md:text-webDesk text-mobileDesk text-gray line-clamp-1 md:line-clamp-2`}
         >
-          {removeHTMLTags(body) || "Lörem ipsum astrobel sar direlig. Kronde est konfoni med kelig. Terabel pov astrobel ?"}
+          {removeHTMLTags(body) ||
+            "Lörem ipsum astrobel sar direlig. Kronde est konfoni med kelig. Terabel pov astrobel ?"}
         </p>
         <div className="flex py-2 md:pt-1 md:pb-0 items-end ">
-          <div className="bg-[#480DEC] rounded-full px-[10px] py-[2px] md:py-1">
-            <p className="text-white md:text-webDesk text-mobileDesk">{tagblog_title}</p>
-          </div>
+          {tags.map((data) => (
+            <div
+              key={data.id}
+              className="bg-[#480DEC] rounded-full px-[10px] py-[2px] md:py-1"
+            >
+              <p className="text-white md:text-webDesk text-mobileDesk">
+                {data.title}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
--- components/LandingPage/Blog/CardBlogSquare.tsx
+++ components/LandingPage/Blog/CardBlogSquare.tsx
@@ -4,23 +4,27 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface BlogProps {
-  id: number,
+  id: number;
   title: string;
-  slug: string,
-  keyword: string,
-  excerpt: string,
-  body: string,
-  altImage: string,
-  kategoriblog_id: number,
-  kategoriblog_title: string,
-  tagblog_id: number,
-  tagblog_title: string,
-  user_id: number,
-  user_title: string,
-  image: any,
-  status: boolean,
-  status_desc: string,
-  publishAt: Date | string,
+  slug: string;
+  keyword: string;
+  excerpt: string;
+  body: string;
+  altImage: string;
+  kategoriblog_id: number;
+  kategoriblog_title: string;
+  tagblog_id: number;
+  tagblog_title: string;
+  user_id: number;
+  user_title: string;
+  image: any;
+  status: boolean;
+  tags: {
+    id: number;
+    title: string;
+  }[];
+  status_desc: string;
+  publishAt: Date | string;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -31,33 +35,49 @@ interface CardBlogProps {
 }
 
 const CardBlogSquare = ({ blogs = {} as BlogProps, type }: CardBlogProps) => {
-  const { id, title, body, image, user_title, publishAt, slug, excerpt, kategoriblog_title, altImage, tagblog_title } = blogs;
+  const {
+    id,
+    title,
+    body,
+    image,
+    user_title,
+    publishAt,
+    slug,
+    excerpt,
+    kategoriblog_title,
+    altImage,
+    tagblog_title,
+    tags,
+  } = blogs;
 
   return (
     <div
       data-aos="fade-up"
       data-aos-duration="1000"
-      className={`${type === "landing"
-        ? "bg-[#14141F] text-white flex-row md:flex-col rounded-[6px] md:rounded-[10px] md:p-5 w-full h-[104px] md:h-auto shadow-sm gap-3 border-[0.5px]"
-        : "bg-transparent flex-row md:w-[410px] h-full rounded-[2px]"
-        } flex gap-3`}
+      className={`${
+        type === "landing"
+          ? "bg-[#14141F] text-white flex-row md:flex-col rounded-[6px] md:rounded-[10px] md:p-5 w-full h-[104px] md:h-auto shadow-sm gap-3 border-[0.5px]"
+          : "bg-transparent flex-row md:w-[410px] h-full rounded-[2px]"
+      } flex gap-3`}
     >
       <div
-        className={`${type === "landing"
-          ? "md:w-full w-[30%] md:h-[233px] min-h-[104px] rounded-l-[6px] md:rounded-[10px]"
-          : "rounded-[2px] w-4/12 md:w-5/12"
-          }`}
+        className={`${
+          type === "landing"
+            ? "md:w-full w-[30%] md:h-[233px] min-h-[104px] rounded-l-[6px] md:rounded-[10px]"
+            : "rounded-[2px] w-4/12 md:w-5/12"
+        }`}
       >
         <Image
           loading="lazy"
-          src={image || '/assets/images/blog.jpg'}
+          src={image || "/assets/images/blog.jpg"}
           alt={altImage || "blog"}
           width={326}
           height={233}
-          className={`${type === "landing"
-            ? "md:h-[300px] h-full object-cover md:rounded-[10px] rounded-l-[6px]"
-            : "rounded-[2px] md:w-full md:h-[150px] object-cover"
-            }
+          className={`${
+            type === "landing"
+              ? "md:h-[300px] h-full object-cover md:rounded-[10px] rounded-l-[6px]"
+              : "rounded-[2px] md:w-full md:h-[150px] object-cover"
+          }
           `}
         />
       </div>
@@ -66,9 +86,13 @@ const CardBlogSquare = ({ blogs = {} as BlogProps, type }: CardBlogProps) => {
       >
         <div className="flex items-center justify-between gap-1.5 text-white">
           <div className="flex items-center gap-1 md:gap-2">
-            <h5 className="md:text-webDesk text-mobileDesk capitalize">{user_title ? user_title : 'Lorenka'}</h5>
+            <h5 className="md:text-webDesk text-mobileDesk capitalize">
+              {user_title ? user_title : "Lorenka"}
+            </h5>
             <div className="rounded-full w-[3px] h-[3px] md:w-[5px] md:h-[5px] bg-white"></div>
-            <h5 className="md:text-webDesk text-mobileDesk">{publishAt ? formattedDate(publishAt) : 'January 13, 2024'}</h5>
+            <h5 className="md:text-webDesk text-mobileDesk">
+              {publishAt ? formattedDate(publishAt) : "January 13, 2024"}
+            </h5>
           </div>
           <Link className="block md:hidden" href={`/${slug}`}>
             <Image
@@ -81,8 +105,11 @@ const CardBlogSquare = ({ blogs = {} as BlogProps, type }: CardBlogProps) => {
           </Link>
         </div>
         <div className="flex items-start w-full md:gap-2 my-1 md:my-3 justify-between">
-          <Link className="font-medium text-white md:text-webSubjudul text-mobileSubjudul mb-1 hover:underline text-wrap line-clamp-2 w-[95%]" href={`/${slug}`}>
-            {title || 'Lorem Ipsum'}
+          <Link
+            className="font-medium text-white md:text-webSubjudul text-mobileSubjudul mb-1 hover:underline text-wrap line-clamp-2 w-[95%]"
+            href={`/${slug}`}
+          >
+            {title || "Lorem Ipsum"}
           </Link>
           <Link className="hidden md:block w-[5%]" href={`/${slug}`}>
             <Image
@@ -97,15 +124,23 @@ const CardBlogSquare = ({ blogs = {} as BlogProps, type }: CardBlogProps) => {
         <p
           className={`${type === "landing" ? "md:text-webDesk w-full" : "w-full -mt-3"} text-mobileDesk md:text-webDesk text-gray line-clamp-1 md:line-clamp-1`}
         >
-          {removeHTMLTags(body) || "Lörem ipsum astrobel sar direlig. Kronde est konfoni med kelig. Terabel pov astrobel ?"}
+          {removeHTMLTags(body) ||
+            "Lörem ipsum astrobel sar direlig. Kronde est konfoni med kelig. Terabel pov astrobel ?"}
         </p>
         <div className="flex gap-1 mt-3 md:items-end">
-          <div className="bg-[#480DEC] rounded-full px-[10px] py-[2px] md:py-1">
-            <p className="text-white md:text-webDesk text-mobileDesk">{tagblog_title || "Lörem ipsum"}</p>
-          </div>
+          {tags.map((data) => (
+            <div
+              key={data.id}
+              className="bg-[#480DEC] rounded-full px-[10px] py-[2px] md:py-1"
+            >
+              <p className="text-white md:text-webDesk text-mobileDesk">
+                {data.title}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
