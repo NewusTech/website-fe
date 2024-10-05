1871e02e3d979b04ee0dad32d8ae2428d77999e2
sakatimuna7 <sakatimunapro7@gmail.com>
2024-10-05 11:25:48 +0700

    landing menambah konten why choose us

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,12 +34,16 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const seoGtm = (await getSeo())[0];
+  console.log(seoGtm)
   return (
     <html lang="en">
       <head>
         {/* GTM Script */}
-        <Script id="GTM">
+        <Script id="GT">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${seoGtm.tagManager}');`}
+        </Script>
+        <Script id="GTM">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${seoGtm.googleTagManager}');`}
         </Script>
       </head>
       <body className={inter.className}>
@@ -49,6 +53,14 @@ export default async function RootLayout({
         <noscript>
           <iframe
             src={`https://www.googletagmanager.com/ns.html?id=${seoGtm.tagManager}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${seoGtm.googleTagManager}`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
--- components/LandingPage/WhyChooseUs/ServiceCard.tsx
+++ components/LandingPage/WhyChooseUs/ServiceCard.tsx
@@ -1,39 +1,132 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 const ServiceCard = ({ icon, title, description }: any) => {
   return (
-    <div className="bg-white rounded-l-lg p-3 sm:p-5 xl:p-6 text-center shadow-lg border-[0.5px] border-gray-300 w-full h-full flex flex-col justify-start lg:justify-center">
+    <div className="bg-white rounded-tl-lg lg:rounded-none p-3 sm:p-5 xl:p-6 text-center shadow-lg border-[0.5px] border-gray-300 w-full h-full flex flex-col justify-start lg:justify-center lg:py-10">
       <div className="mb-4 flex justify-center">{icon}</div>
-      <h3 className="text-mobileSubjudul md:text-webSubjudul font-semibold mb-2">{title}</h3>
-      <p className="text-mobileDesk md:text-webDesk text-gray-600">{description}</p>
+      <h3 className="text-mobileSubjudul md:text-webSubjudul font-semibold mb-2">
+        {title}
+      </h3>
+      <p className="text-mobileDesk md:text-webDesk text-gray-600">
+        {description}
+      </p>
     </div>
   );
 };
 
 const ServiceCard2 = ({ icon, title, description }: any) => {
   return (
-    <div className="bg-white rounded-r-lg p-3 sm:p-5 xl:p-6 text-center shadow-lg border-[0.5px] border-gray-300 w-full h-full flex flex-col justify-start lg:justify-center">
+    <div className="bg-white rounded-tr-lg lg:rounded-none p-3 sm:p-5 xl:p-6 text-center shadow-lg border-[0.5px] border-gray-300 w-full h-full flex flex-col justify-start lg:justify-center lg:py-10">
       <div className="mb-4 flex justify-center">{icon}</div>
-      <h3 className="text-mobileSubjudul md:text-webSubjudul font-semibold mb-2">{title}</h3>
-      <p className="text-mobileDesk md:text-webDesk text-gray-600">{description}</p>
+      <h3 className="text-mobileSubjudul md:text-webSubjudul font-semibold mb-2">
+        {title}
+      </h3>
+      <p className="text-mobileDesk md:text-webDesk text-gray-600">
+        {description}
+      </p>
     </div>
   );
 };
 
 const Services = () => {
   return (
-    <div className="flex bg-gray-100 rounded-lg h-[160px] lg:h-full">
+    // <div className="flex bg-gray-100 rounded-lg h-[160px] lg:h-full">
+    <div className="grid grid-cols-2 lg:grid-cols-3 lg:gap-4 bg-gray-100 gap-0">
       <ServiceCard
-        icon={<Image alt='Certified Engineers' src='/assets/icons/about-landing/Certified.svg' width={50} height={50} className='w-[30px] lg:w-[50px]' />}
+        icon={
+          <Image
+            alt="Certified Engineers"
+            src="/assets/icons/about-landing/Certified.svg"
+            width={50}
+            height={50}
+            className="w-[30px] lg:w-[50px]"
+          />
+        }
         title="Certified Engineers"
         description="Deep expertise and experience for optimal and reliable results."
       />
       <ServiceCard2
-        icon={<Image alt='Delivery on time' src='/assets/icons/about-landing/Delivery.svg' width={50} height={50} className='w-[30px] lg:w-[50px]' />}
+        icon={
+          <Image
+            alt="Delivery on time"
+            src="/assets/icons/about-landing/Delivery.svg"
+            width={50}
+            height={50}
+            className="w-[30px] lg:w-[50px]"
+          />
+        }
         title="Delivery on time"
         description="Ensure every IT solution is completed on schedule to support your business success."
       />
+      <div className="bg-white p-3 sm:p-5 xl:p-6 text-center shadow-lg border-[0.5px] border-gray-300 w-full h-full flex flex-col justify-start lg:justify-center lg:py-10">
+        <div className="mb-4 flex justify-center">
+          <Image
+            alt="Warranty"
+            src="/assets/icons/about-landing/Service.svg"
+            width={50}
+            height={50}
+            className="w-[30px] lg:w-[50px]"
+          />
+        </div>
+        <h3 className="text-mobileSubjudul md:text-webSubjudul font-semibold mb-2">
+          Warranty
+        </h3>
+        <p className="text-mobileDesk md:text-webDesk text-gray-600">
+          1 year service warranty
+        </p>
+      </div>
+      <div className="bg-white p-3 sm:p-5 xl:p-6 text-center shadow-lg border-[0.5px] border-gray-300 w-full h-full flex flex-col justify-start lg:justify-center lg:py-10">
+        <div className="mb-4 flex justify-center">
+          <Image
+            alt="Money back guarantee"
+            src="/assets/icons/about-landing/money-bag.svg"
+            width={50}
+            height={50}
+            className="w-[30px] lg:w-[50px]"
+          />
+        </div>
+        <h3 className="text-mobileSubjudul md:text-webSubjudul font-semibold mb-2">
+          Money Back Guarantee
+        </h3>
+        <p className="text-mobileDesk md:text-webDesk text-gray-600">
+          100% money back guarantee if our work does not meet your needs
+        </p>
+      </div>
+      <div className="bg-white rounded-bl-lg p-3 sm:p-5 xl:p-6 text-center shadow-lg border-[0.5px] border-gray-300 w-full h-full flex flex-col justify-start lg:justify-center lg:py-10">
+        <div className="mb-4 flex justify-center">
+          <Image
+            alt="Delivery on time"
+            src="/assets/icons/about-landing/certificate-bold.svg"
+            width={50}
+            height={50}
+            className="w-[30px] lg:w-[50px]"
+          />
+        </div>
+        <h3 className="text-mobileSubjudul md:text-webSubjudul font-semibold mb-2">
+          Experienced Technician
+        </h3>
+        <p className="text-mobileDesk md:text-webDesk text-gray-600">
+          Experienced technicians more than 12 years
+        </p>
+      </div>
+      <div className="bg-white rounded-br-lg p-3 sm:p-5 xl:p-6 text-center shadow-lg border-[0.5px] border-gray-300 w-full h-full flex flex-col justify-start lg:justify-center lg:py-10">
+        <div className="mb-4 flex justify-center">
+          <Image
+            alt="Delivery on time"
+            src="/assets/icons/about-landing/collaborate.svg"
+            width={50}
+            height={50}
+            className="w-[30px] lg:w-[50px]"
+          />
+        </div>
+        <h3 className="text-mobileSubjudul md:text-webSubjudul font-semibold mb-2">
+          Customer Satisfaction
+        </h3>
+        <p className="text-mobileDesk md:text-webDesk text-gray-600">
+          Quality and client satisfaction are our priorities
+        </p>
+      </div>
     </div>
   );
 };
